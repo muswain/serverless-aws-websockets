5ae127e8b22ca6f42a8353a935f8255c6b5571bb
mrutyunjay <mrutyunjay.swain@gmail.com>
2021-04-05 07:12:59 +0800

    added handler files and updated serverless yml

--- src/helpers/dynamodb.spec.ts
+++ src/helpers/dynamodb.spec.ts
@@ -1,7 +1,43 @@
-import { expect } from 'chai';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { expect, use } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+use(sinonChai);
+import { queryUsers } from './dynamodb';
 
 describe('dynamodb helper', () => {
   describe('queryUsers', () => {
-    expect(1).to.equal(1);
+    let queryStub: sinon.SinonStub;
+    const sandbox = sinon.createSandbox();
+
+    before(() => (process.env.USER_TABLE = 'users'));
+
+    beforeEach(() => {
+      process.env.USER_TABLE = 'users';
+      queryStub = sandbox.stub(DynamoDBDocumentClient.prototype, 'send');
+    });
+
+    afterEach(() => sandbox.restore());
+
+    it('should query ddb and get list of users', async () => {
+      queryStub.resolves({ Items: [{ pk: 'mj', name: 'mswain', city: 'perth', country: 'australia' }] });
+      const response = await queryUsers('test user');
+      expect(response).to.deep.equal([{ name: 'mswain', city: 'perth', country: 'australia', email: 'mj' }]);
+    });
+
+    it('should query ddb with the required params', async () => {
+      queryStub.resolves({ Items: [] });
+      await queryUsers('test user');
+      expect(queryStub).to.have.been.calledOnce;
+      expect(queryStub).to.have.been.calledWithMatch({
+        input: {
+          TableName: 'users',
+          IndexName: 'sk-name',
+          KeyConditionExpression: 'sk = :pk AND begins_with(#user, :name)',
+          ExpressionAttributeNames: { '#user': 'name' },
+          ExpressionAttributeValues: { ':pk': 'profile', ':name': 'test user' },
+        },
+      });
+    });
   });
 });
